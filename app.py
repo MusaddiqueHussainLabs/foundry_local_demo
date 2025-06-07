e2f644349d030787e5a3a16066df9e083acee60a
MusaddiqueHussainLabs <musaddiquehussainlabs@gmail.com>
2025-06-07 19:35:27 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+# import openai
+# from foundry_local import FoundryLocalManager
+
+# # By using an alias, the most suitable model will be downloaded 
+# # to your end-user's device. 
+# alias = "phi-3.5-mini"
+
+# # Create a FoundryLocalManager instance. This will start the Foundry
+# # Local service if it is not already running and load the specified model.
+# manager = FoundryLocalManager(alias)
+# # The remaining code uses the OpenAI Python SDK to interact with the local model.
+# # Configure the client to use the local Foundry service
+# client = openai.OpenAI(
+#     base_url=manager.endpoint,
+#     api_key=manager.api_key  # API key is not required for local usage
+# )
+# # Set the model to use and generate a response
+# response = client.chat.completions.create(
+#     model=manager.get_model_info(alias).id,
+#     messages=[{"role": "user", "content": "What is machine learning"}]
+# )
+# print(response.choices[0].message.content)
+
+import openai
+from foundry_local import FoundryLocalManager
+
+# By using an alias, the most suitable model will be downloaded 
+# to your end-user's device.
+alias = "phi-3.5-mini"
+
+# Create a FoundryLocalManager instance. This will start the Foundry 
+# Local service if it is not already running and load the specified model.
+manager = FoundryLocalManager(alias)
+
+# The remaining code us es the OpenAI Python SDK to interact with the local model.
+
+# Configure the client to use the local Foundry service
+client = openai.OpenAI(
+    base_url=manager.endpoint,
+    api_key=manager.api_key  # API key is not required for local usage
+)
+
+# Set the model to use and generate a streaming response
+stream = client.chat.completions.create(
+    model=manager.get_model_info(alias).id,
+    messages=[{"role": "user", "content": "What is machine learning?"}],
+    stream=True
+)
+
+# Print the streaming response
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="", flush=True)
